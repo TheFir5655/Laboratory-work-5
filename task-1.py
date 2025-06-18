342fcc658f560abc550134edaceceacabdc43d83
TheFir <artemsevlakov18@gmail.com>
2025-06-18 14:34:57 +0700

    Delete task-1.py

--- task-1.py
+++
@@ -1,7 +0,0 @@
-list = ['Артём', 'Денис', 'Абоба', 'Максим', 'Алег', 'Олег', 'Илья', 'Сергей', 'Вергилий', 'ОН']
-A_names = []
-for i in list:
-    for j in i:
-        if j[0] == 'А':
-            A_names.append(i)
-print(A_names)
