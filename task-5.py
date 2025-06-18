d786c0706fa9ce97b2e5358911b3504a5db05968
TheFir <artemsevlakov18@gmail.com>
2025-06-18 14:35:34 +0700

    Delete task-5.py

--- task-5.py
+++
@@ -1,8 +0,0 @@
-c = "C"
-A = ["CaaaC", "sus", "C", "COOL", "COOLREALLYCOOLC"]
-amount = 0
-for i in A:
-    if len(i) > 1 and i.startswith(c) and i.endswith(c):
-        print(i)
-        amount += 1
-print(amount)
